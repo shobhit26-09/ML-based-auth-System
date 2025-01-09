c186d48d5060014d898936f35607468e0802e630
shobhit26-09 <123019122+shobhit26-09@users.noreply.github.com>
2025-01-09 21:15:38 +0530

    Create feature_utils.py

---
+++ feature_utils.py
@@ -0,0 +1,16 @@
+import librosa
+import numpy as np
+
+def extract_features(audio, sr=44100):
+    mfccs = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=13)
+    chroma = librosa.feature.chroma_stft(y=audio, sr=sr)
+    mel = librosa.feature.melspectrogram(y=audio, sr=sr)
+    contrast = librosa.feature.spectral_contrast(y=audio, sr=sr)
+    
+    features = np.hstack([
+        np.mean(mfccs.T, axis=0),
+        np.mean(chroma.T, axis=0),
+        np.mean(mel.T, axis=0),
+        np.mean(contrast.T, axis=0)
+    ])
+    return features
